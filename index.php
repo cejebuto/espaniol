da09ea23d046a0e74d265044ca578e0e42a00658
Cesar Buelvas <cejebuto@gmail.com>
2013-09-14 16:08:11 -0500

    First Commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+Theme Name: Repurpose
+Theme URI: http://osclass.org
+Description: OSClass theme using twitter bootstrap 1.4
+Version: 1.2
+Author: OSClass team
+Author URI: http://osclass.org
+Widgets: header,footer
+Theme update URI: repurpose
+*/
+    function repurpose_theme_info() {
+        return array(
+             'name'        => 'Español'
+            ,'version'     => '1.2'
+            ,'description' => 'Thema de osclass con menú al español'
+            ,'author_name' => 'Cesar Buelvas'
+            ,'author_url'  => ''
+            ,'locations'   => array('header', 'footer')
+        );
+    }
+
+?>
